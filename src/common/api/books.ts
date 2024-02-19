4a7369f671258f55a2b6555a5d8bed705c7fdfaa
rakhman <r.abay@gmail.com>
2024-02-19 19:22:44 +0600

    books react-query api hooks (create/update/getAll)

---
+++ src/common/api/books.ts
@@ -0,0 +1,31 @@
+import { useMutation, useQuery, useQueryClient } from "react-query";
+import IBook from "../types/IBook";
+import apiClient from "./apiClient";
+
+const queryClient = useQueryClient();
+const BOOK_LIST_QUERY_KEY = ["books"];
+const CREATE_BOOK_QUERY_KEY = "createBook";
+
+const fetchBooks = async (): Promise<IBook[]> => {
+    const { data } = await apiClient.books.getBooks();
+    return data;
+};
+export const useGetBooks = () => {
+    return useQuery(BOOK_LIST_QUERY_KEY, () => fetchBooks());
+};
+
+const createOrUpdateBook = async (payload: IBook) => {
+    const { data } = await apiClient.books.createOrUpdate(payload);
+    return data;
+};
+export const useCreateOrUpdateBook = () => {
+    return useMutation<IBook, Error, IBook>(createOrUpdateBook, {
+        onSuccess: (bookData) => {
+            // invalidate the query cache for 'books'
+            queryClient.invalidateQueries(BOOK_LIST_QUERY_KEY);
+        },
+        onError: (error) => {
+            // handle error
+        },
+    });
+};
