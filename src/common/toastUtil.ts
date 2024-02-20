d6be01050d1d07221f73bfa1e89196384aaf2032
rakhman <r.abay@gmail.com>
2024-02-20 18:01:04 +0600

    toast design change

--- src/common/toastUtil.ts
+++ src/common/toastUtil.ts
@@ -3,27 +3,27 @@ import { toast } from "react-toastify";
 const toastUtil = {
     success: (content: string) => {
         toast.success(content, {
-            position: "bottom-right",
+            position: "top-right",
             autoClose: 5000,
             hideProgressBar: false,
             closeOnClick: true,
             pauseOnHover: true,
             draggable: true,
             progress: undefined,
-            theme: "light",
+            theme: "colored",
         });
     },
 
     error: (content: string) => {
         toast.error(content, {
-            position: "bottom-right",
+            position: "top-right",
             autoClose: 5000,
             hideProgressBar: false,
             closeOnClick: true,
             pauseOnHover: true,
             draggable: true,
             progress: undefined,
-            theme: "light",
+            theme: "colored",
         });
     },
 };
