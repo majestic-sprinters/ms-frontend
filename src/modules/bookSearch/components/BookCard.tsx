8c84c9c1302e3724240cee28b117a481401e061e
rakhman <r.abay@gmail.com>
2024-02-20 23:35:59 +0600

    book card extend data

--- src/modules/bookSearch/components/BookCard.tsx
+++ src/modules/bookSearch/components/BookCard.tsx
@@ -7,8 +7,12 @@ interface Props {
 const BookCard = ({ book }: Props) => {
     return (
         <>
-            <h1>Book Card</h1>
-            <h1>{book.name}</h1>
+            <h1>Book</h1>
+            <h1>Name: {book.name}</h1>
+            <h1>Description: {book.description}</h1>
+            <h1>Author: {book.author}</h1>
+            <h1>Publisher: {book.publisher}</h1>
+            <h1>Year: {book.year}</h1>
         </>
     );
 };
