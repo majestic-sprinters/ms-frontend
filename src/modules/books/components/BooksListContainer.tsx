b9a318ed54aa5a624cb74c96d6bf0e0ddef2e9b3
rakhman <r.abay@gmail.com>
2024-02-20 16:53:39 +0600

    add container to booklist for queries

---
+++ src/modules/books/components/BooksListContainer.tsx
@@ -0,0 +1,36 @@
+import BooksList from "./BooksList";
+import { useGetBooks } from "../../../common/api/books";
+import toastUtil from "../../../common/toastUtil";
+import { List, Typography } from "antd";
+
+const BooksListContainer = (): JSX.Element => {
+    const {
+        data: books,
+        isLoading: isBooksLoading,
+        isError: isBooksError,
+    } = useGetBooks();
+
+    if (isBooksLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (isBooksError) {
+        toastUtil.error("Error fetching books");
+        return (
+            <List
+                header={
+                    <Typography.Title level={5}>
+                        Error fetching books
+                    </Typography.Title>
+                }
+                bordered
+                dataSource={[]}
+                renderItem={(item) => <List.Item></List.Item>}
+            />
+        );
+    }
+
+    return <BooksList books={books ?? []} />;
+};
+
+export default BooksListContainer;
